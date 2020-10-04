8446c87449648e7e27ee5e5da5ce53dd88bd0acc
Tom <tanewton95@gmail.com>
2020-10-04 17:04:11 +0100

    flutter_chat_ui home

--- flutter_chat_ui/lib/models/message_model.dart
+++ flutter_chat_ui/lib/models/message_model.dart
@@ -8,7 +8,125 @@ class Message {
   final bool unread;
 
   Message({this.sender, this.time, this.text, this.isLiked, this.unread});
-
-  final User currentUser =
-      User(id: 0, name: 'Current User', imageUrl: 'assets/images/greg.png');
 }
+
+final User currentUser =
+    User(id: 0, name: 'Tom', imageUrl: 'assets/images/greg.png');
+
+final User greg = User(id: 1, name: 'Greg', imageUrl: 'assets/images/greg.png');
+
+final User james =
+    User(id: 2, name: 'James', imageUrl: 'assets/images/james.png');
+
+final User john = User(id: 3, name: 'John', imageUrl: 'assets/images/john.png');
+
+final User olivia =
+    User(id: 4, name: 'Olivia', imageUrl: 'assets/images/olivia.png');
+
+final User sam = User(id: 5, name: 'Sam', imageUrl: 'assets/images/sam.png');
+
+final User sophia =
+    User(id: 6, name: 'Sophia', imageUrl: 'assets/images/sophia.png');
+
+final User steven =
+    User(id: 7, name: 'Steven', imageUrl: 'assets/images/steven.png');
+
+List<User> favourites = [sam, steven, olivia, john, greg];
+
+List<Message> chats = [
+  Message(
+    sender: james,
+    time: '5:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: olivia,
+    time: '4:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: john,
+    time: '3:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: sophia,
+    time: '2:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: steven,
+    time: '1:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: sam,
+    time: '12:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+  Message(
+    sender: greg,
+    time: '11:30 AM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: false,
+    unread: false,
+  ),
+];
+
+// EXAMPLE MESSAGES IN CHAT SCREEN
+List<Message> messages = [
+  Message(
+    sender: james,
+    time: '5:30 PM',
+    text: 'Hey, how\'s it going? What did you do today?',
+    isLiked: true,
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '4:30 PM',
+    text: 'Just walked my doge. She was super duper cute. The best pupper!!',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: james,
+    time: '3:45 PM',
+    text: 'How\'s the doggo?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: james,
+    time: '3:15 PM',
+    text: 'All the food',
+    isLiked: true,
+    unread: true,
+  ),
+  Message(
+    sender: currentUser,
+    time: '2:30 PM',
+    text: 'Nice! What kind of food did you eat?',
+    isLiked: false,
+    unread: true,
+  ),
+  Message(
+    sender: james,
+    time: '2:00 PM',
+    text: 'I ate so much food today.',
+    isLiked: false,
+    unread: true,
+  ),
+];
--- flutter_chat_ui/lib/screens/home_screen.dart
+++ flutter_chat_ui/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/widgets/category_selector.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,43 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0))),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
